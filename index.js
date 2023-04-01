240b9c98324a46af55da96374be0f4c7d3eb8e19
Davi Marques <Davichamico@gmail.com>
2023-04-01 19:44:20 -0300

    arrumando alguns erros

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.turbo-911', { duration: 3000 });
 
-sr.reveal('.cayman-718', { duration: 3000 });
+sr.reveal('h2', { duration: 1000 });
+
+
+sr.reveal('.cayman-718', { duration: 4000 });
