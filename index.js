a23d4cf55fdc5ce12dcd57e887b2b7fc8b5860a1
Davi Marques <Davichamico@gmail.com>
2023-04-03 22:50:53 -0300

    mudando as imagens a adicionando efeitos visuais, junto com a seÇão '911'

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.turbo-911', { duration: 3000 });
+sr.reveal('.turbo-911', { duration: 5000 });
 
 sr.reveal('h2', { duration: 1000 });
 
-
-sr.reveal('.cayman-718', { duration: 4000 });
+sr.reveal('.cayman-718', { duration: 5000 });
